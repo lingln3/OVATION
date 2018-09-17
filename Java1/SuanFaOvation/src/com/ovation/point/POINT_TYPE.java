ec450562a1fca603b849829e8e28f19870a47fa6
gln <lingln68@gmail.com>
2018-09-17 17:07:52 +0800

    OVATION算法功能实现的代码更新

---
+++ Java1/SuanFaOvation/src/com/ovation/point/POINT_TYPE.java
@@ -0,0 +1,11 @@
+package com.ovation.point;
+/**
+ * 枚举出点的类型：模拟量点，数字量点，打包点
+ * @author snpas
+ *
+ */
+public enum POINT_TYPE {
+	LA,LD,LP;
+	//成员变量，设定不同类型的点拥有的数值类型
+	
+}
---
+++ Java1/SuanFaOvation/src/com/ovation/point/QUALITY.java
@@ -0,0 +1,5 @@
+package com.ovation.point;
+
+public enum QUALITY {
+	GOOD,FAIL,POOR,BAD
+}
